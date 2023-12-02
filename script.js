c7b921f0d25cda2dab2e86290baea2df4a81cf5d
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-02 19:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+      const checkboxes = document.querySelectorAll('.max-two');
+
+      checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+          if (this.checked) {
+            checkboxes.forEach(otherCheckbox => {
+              if (otherCheckbox !== this) {
+                otherCheckbox.checked = false;
+              }
+            });
+          }
+
+          this.parentElement.classList.toggle(this.id);
+        });
+      });
+    });
